fed2ba0c89fad41cf41e5a7143bbf31d5368fbcb
charlielew1s <91842295+charlielew1s@users.noreply.github.com>
2024-01-04 15:29:55 +0000

    reset

--- command-line-app/src/App.js
+++ command-line-app/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-container">
+          <p className="App-name">Charlie Lewis</p>
+          <div className="line-breaks">
+            {[...Array(1)].map((_, index) => (
+              <br key={index} />
+            ))}
+          </div>
+          <p className="App-title">Full stack software engineer</p>
+        </div>
       </header>
     </div>
   );
